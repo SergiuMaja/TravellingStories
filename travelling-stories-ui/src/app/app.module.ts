b495d4a0f3c4d35b8df24e402041aaab20cb498b
SergiuMaja <Sergiu.Maja@student.utcluj.ro>
2018-02-05 19:57:35 +0200

    Make a responsive navbar + create the signup and signin components

--- travelling-stories-ui/src/app/app.module.ts
+++ travelling-stories-ui/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
