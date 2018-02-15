9b9e731e9f138f7c70ff39de23f5bbe210c940c9
SergiuMaja <Sergiu.Maja@student.utcluj.ro>
2018-02-15 15:03:54 +0200

    Fill the story details template with data

--- travelling-stories-ui/src/app/stories/story-detail/story-detail.component.ts
+++ travelling-stories-ui/src/app/stories/story-detail/story-detail.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {Story} from "../story.model";
+import {StoryService} from "../story.service";
+import {User} from "../../users/user.model";
+import {Subscription} from "rxjs";
+import {DescriptionService} from "../description.service";
 
 @Component({
   selector: 'app-story-detail',
   templateUrl: './story-detail.component.html',
   styleUrls: ['./story-detail.component.css']
 })
-export class StoryDetailComponent implements OnInit {
+export class StoryDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  storyId: number;
+  story: Story;
+  creator: User = new User();
+  creatorChangedSubscription: Subscription;
+  lat: number = 46.7428338;
+  lng: number = 23.5009179;
+  locationPinned: boolean = false;
+
+  constructor(private route: ActivatedRoute,
+              private storyService: StoryService,
+              private descriptionService: DescriptionService) {}
 
   ngOnInit() {
+    this.creatorChangedSubscription = this.storyService.creatorChanged.subscribe(
+      (creator: User) => {
+        this.creator = creator;
+      }
+    );
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.storyId = +params['id'];
+        this.story = this.storyService.getStory(this.storyId);
+        this.storyService.getStoryCreator(this.story.creatorId);
+        this.lat = +this.story.destination.latitude;
+        this.lng = +this.story.destination.longitude;
+        this.locationPinned = this.lat != null && this.lng != null;
+      }
+    );
+  }
+
+  getDescriptionDetailsByType(type: string) {
+    return this.descriptionService.getDescriptionByType(type, this.story.descriptions).details;
+  }
+
+  ngOnDestroy() {
+    this.creatorChangedSubscription.unsubscribe();
   }
 
 }
